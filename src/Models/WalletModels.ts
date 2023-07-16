53fcd58ae18d42d73489bd855469122bf2fdb5df
nicsylvia <nicsylvia15f@gmail.com>
2023-07-15 23:42:03 -0700

    wallet models created

---
+++ src/Models/WalletModels.ts
@@ -0,0 +1,27 @@
+import mongoose, { model, Schema, Document } from "mongoose";
+import { WalletData } from "../Interfaces/AllInterfaces";
+
+interface MainWalletData extends WalletData, Document {}
+
+const WalletSchema = new Schema<WalletData>(
+  {
+    Owner: {
+      type: String,
+    },
+    Balance: {
+      type: Number,
+    },
+    debit: {
+      type: Number,
+    },
+    credit: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const WalletModels = model<MainWalletData>("Users-Wallets", WalletSchema);
+export default WalletModels;
