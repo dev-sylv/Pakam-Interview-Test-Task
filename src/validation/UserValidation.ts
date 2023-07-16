023632f6a4897e2add331f95b550a17a6af94d21
nicsylvia <nicsylvia15f@gmail.com>
2023-07-15 23:26:34 -0700

    User validation done

---
+++ src/validation/UserValidation.ts
@@ -0,0 +1,19 @@
+import validationSetUp from "../validatorSetUp";
+import { NextFunction, Request, Response, RequestHandler } from "express";
+import { UserSchemaValidation } from "./UserSchema";
+
+export const ValidateUserSignUp: RequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  validationSetUp(UserSchemaValidation.UserSignUp, req.body, next);
+};
+
+export const ValidateUserLogin: RequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  validationSetUp(UserSchemaValidation.UserLogin, req.body, next);
+};
